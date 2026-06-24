e66bd908ce7f2b2329432104d515ec84e64d0dcd
vihangadinethmi <vihangadinethmi12@gmail.com>
2026-06-24 23:15:09 +0530

    supplier

---
+++ src/main/java/com/stockmanagement/model/Supplier.java
@@ -0,0 +1,26 @@
+package com.stockmanagement.model;
+
+public class Supplier {
+    private int id;
+    private String name;
+    private String contact;
+    private String email;
+
+    public Supplier() {}
+
+    public Supplier(int id, String name, String contact, String email) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getContact() { return contact; }
+    public void setContact(String contact) { this.contact = contact; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
